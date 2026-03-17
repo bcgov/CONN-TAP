69261b1b98b4256cdbf452623f5e9cd6d6ed1fe8
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2026-03-17 14:58:08 -0700

    chore: lambda names, vars, add docs

--- infra/envs/test/terraform.tfvars
+++ infra/envs/test/terraform.tfvars
@@ -1,7 +1,7 @@
-aws_region = "ca-central-1"
-
-license = "dd5a29"
-env     = "test"
+# Will be set via environment variable
+# aws_region = "ca-central-1"
+# license = "dd5a29"
+# env     = "test"
 
 # Defaults are set in variables.tf — uncomment below to override:
 # ec2_security_group_names = ["App"]
--- infra/modules/dmp_shared/s3_objects.tf
+++ infra/modules/dmp_shared/s3_objects.tf
@@ -15,16 +15,16 @@ resource "aws_s3_object" "glue_scripts" {
 
 data "archive_file" "lambda_ngta_rogers" {
   type        = "zip"
-  output_path = "${var.assets_dir}/lambda/lambda-ngta-rogers.zip"
+  output_path = "${var.assets_dir}/lambda/lambda_ngta_rogers.zip"
   source {
-    content  = file("${var.assets_dir}/lambda/lambda-ngta-rogers.py")
+    content  = file("${var.assets_dir}/lambda/lambda_ngta_rogers.py")
     filename = "lambda_function.py"
   }
 }
 
 resource "aws_s3_object" "lambda_ngta_rogers_zip" {
   bucket       = aws_s3_bucket.glue_assets.bucket
-  key          = "lambda/lambda-ngta-rogers.zip"
+  key          = "lambda/lambda_ngta_rogers.zip"
   source       = data.archive_file.lambda_ngta_rogers.output_path
   etag         = data.archive_file.lambda_ngta_rogers.output_md5
   content_type = "application/zip"
@@ -32,16 +32,16 @@ resource "aws_s3_object" "lambda_ngta_rogers_zip" {
 
 data "archive_file" "lambda_ngta_telus" {
   type        = "zip"
-  output_path = "${var.assets_dir}/lambda/lambda-ngta-telus.zip"
+  output_path = "${var.assets_dir}/lambda/lambda_ngta_telus.zip"
   source {
-    content  = file("${var.assets_dir}/lambda/lambda-ngta-telus.py")
+    content  = file("${var.assets_dir}/lambda/lambda_ngta_telus.py")
     filename = "lambda_function.py"
   }
 }
 
 resource "aws_s3_object" "lambda_ngta_telus_zip" {
   bucket       = aws_s3_bucket.glue_assets.bucket
-  key          = "lambda/lambda-ngta-telus.zip"
+  key          = "lambda/lambda_ngta_telus.zip"
   source       = data.archive_file.lambda_ngta_telus.output_path
   etag         = data.archive_file.lambda_ngta_telus.output_md5
   content_type = "application/zip"
@@ -49,16 +49,16 @@ resource "aws_s3_object" "lambda_ngta_telus_zip" {
 
 data "archive_file" "lambda_ngta_telus_quantities" {
   type        = "zip"
-  output_path = "${var.assets_dir}/lambda/lambda-ngta-telus-quantities.zip"
+  output_path = "${var.assets_dir}/lambda/lambda_ngta_telus_quantities.zip"
   source {
-    content  = file("${var.assets_dir}/lambda/lambda-ngta-telus-quantities.py")
+    content  = file("${var.assets_dir}/lambda/lambda_ngta_telus_quantities.py")
     filename = "lambda_function.py"
   }
 }
 
 resource "aws_s3_object" "lambda_ngta_telus_quantities_zip" {
   bucket       = aws_s3_bucket.glue_assets.bucket
-  key          = "lambda/lambda-ngta-telus-quantities.zip"
+  key          = "lambda/lambda_ngta_telus_quantities.zip"
   source       = data.archive_file.lambda_ngta_telus_quantities.output_path
   etag         = data.archive_file.lambda_ngta_telus_quantities.output_md5
   content_type = "application/zip"
@@ -66,16 +66,16 @@ resource "aws_s3_object" "lambda_ngta_telus_quantities_zip" {
 
 data "archive_file" "lambda_tsma_fact" {
   type        = "zip"
-  output_path = "${var.assets_dir}/lambda/lambda-tsma-fact.zip"
+  output_path = "${var.assets_dir}/lambda/lambda_tsma_fact.zip"
   source {
-    content  = file("${var.assets_dir}/lambda/lambda-tsma-fact.py")
+    content  = file("${var.assets_dir}/lambda/lambda_tsma_fact.py")
     filename = "lambda_function.py"
   }
 }
 
 resource "aws_s3_object" "lambda_tsma_fact_zip" {
   bucket       = aws_s3_bucket.glue_assets.bucket
-  key          = "lambda/lambda-tsma-fact.zip"
+  key          = "lambda/lambda_tsma_fact.zip"
   source       = data.archive_file.lambda_tsma_fact.output_path
   etag         = data.archive_file.lambda_tsma_fact.output_md5
   content_type = "application/zip"
@@ -83,16 +83,16 @@ resource "aws_s3_object" "lambda_tsma_fact_zip" {
 
 data "archive_file" "lambda_tsma_qsr" {
   type        = "zip"
-  output_path = "${var.assets_dir}/lambda/lambda-tsma-qsr.zip"
+  output_path = "${var.assets_dir}/lambda/lambda_tsma_qsr.zip"
   source {
-    content  = file("${var.assets_dir}/lambda/lambda-tsma-qsr.py")
+    content  = file("${var.assets_dir}/lambda/lambda_tsma_qsr.py")
     filename = "lambda_function.py"
   }
 }
 
 resource "aws_s3_object" "lambda_tsma_qsr_zip" {
   bucket       = aws_s3_bucket.glue_assets.bucket
-  key          = "lambda/lambda-tsma-qsr.zip"
+  key          = "lambda/lambda_tsma_qsr.zip"
   source       = data.archive_file.lambda_tsma_qsr.output_path
   etag         = data.archive_file.lambda_tsma_qsr.output_md5
   content_type = "application/zip"
